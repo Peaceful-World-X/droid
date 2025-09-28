8a3f95c122674fbb8488c50e8ba875b2a1b36af9
xue.wenyao <xue.wenyao@cytoderm.ai>
2025-09-28 13:20:17 +0800

    update: local config

--- droid/misc/parameters.py
+++ droid/misc/parameters.py
@@ -1,13 +1,14 @@
 import os
+
 from cv2 import aruco
 
 # Robot Params #
-nuc_ip = ""
-robot_ip = ""
-laptop_ip = ""
-sudo_password = ""
-robot_type = ""  # 'panda' or 'fr3'
-robot_serial_number = ""
+nuc_ip = "192.168.40.200"
+robot_ip = "10.0.10.2"
+laptop_ip = "192.168.40.2"
+sudo_password = "Cytoderm2223"
+robot_type = "fr3"  # 'panda' or 'fr3'
+robot_serial_number = "290102-40602"
 
 # Camera ID's #
 hand_camera_id = ""
@@ -22,8 +23,7 @@ CHARUCOBOARD_MARKER_SIZE = 0.016
 ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_5X5_100)
 
 # Ubuntu Pro Token (RT PATCH) #
-ubuntu_pro_token = ""
+ubuntu_pro_token = "C12WmUrvpKfaxvyGy1PBKVF7EYgem8"
 
 # Code Version [DONT CHANGE] #
 droid_version = "1.3"
-
